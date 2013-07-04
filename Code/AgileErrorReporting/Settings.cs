13708fc56d87013b9b4d1674858c104b5899beee
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-07-04 11:48:44 +0300

    added settings

---
+++ Code/AgileErrorReporting/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace AgileErrorReporting
+{
+    public class Settings
+    {
+        public static string ServiceEndPoint { get; set; }
+
+        public static string AppName { get; set; }
+
+        public static string InstanceIdentifier { get; set; }
+    }
+}
