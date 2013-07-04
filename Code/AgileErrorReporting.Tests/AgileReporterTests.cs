0bf2b631b7744f4b507786356063c2d205b63c78
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-07-04 11:53:44 +0300

    added AgileReporter class that returns a singleton

---
+++ Code/AgileErrorReporting.Tests/AgileReporterTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AgileErrorReporting.Tests
+{
+    [TestFixture]
+    public class AgileReporterTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Settings.InstanceIdentifier = "user-token";
+            Settings.AppName = "appName";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Settings.InstanceIdentifier = "user-token";
+            Settings.AppName = "appName";
+            AgileReporter.Instance.Dispose();
+        }
+
+        [Test]
+        public void Instance_Always_ReturnsNotNull()
+        {
+            var instance = AgileReporter.Instance;
+
+            Assert.IsNotNull(instance);
+        }
+
+        [Test]
+        public void Instance_Always_ReturnsSameInstance()
+        {
+            var instance1 = AgileReporter.Instance;
+            var instance2 = AgileReporter.Instance;
+
+            Assert.AreSame(instance1, instance2);
+        }
+
+        [Test]
+        public void Instance_AfterCallingDisposeOnCurrentInstance_ReturnsNewInstance()
+        {
+            var instance1 = AgileReporter.Instance;
+            instance1.Dispose();
+
+            var instance2 = AgileReporter.Instance;
+
+            Assert.AreNotSame(instance1, instance2);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Instance_WhenApiKeyIsNotSet_RaisesArgumentException()
+        {
+            Settings.InstanceIdentifier = null;
+
+            var instance1 = AgileReporter.Instance;
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Instance_WhenAppNameIsNotSet_RaisesArgumentException()
+        {
+            Settings.InstanceIdentifier = "user-token";
+            Settings.AppName = null;
+
+            var instance = AgileReporter.Instance;
+        }
+    }
+}
---
+++ Code/AgileErrorReporting/AgileReporter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AgileErrorReporting
+{
+    public class AgileReporter : IReportingService
+    {
+        private static AgileReporter _instance;
+        public static IReportingService Instance
+        {
+            get
+            {
+                return _instance ?? (_instance = new AgileReporter());
+            }
+        }
+
+        private AgileReporter()
+        {
+            if (string.IsNullOrEmpty(Settings.InstanceIdentifier))
+            {
+                throw new ArgumentException("Instance identifier not set");
+            }
+            if (string.IsNullOrEmpty(Settings.AppName))
+            {
+                throw new ArgumentException("AppName not set");
+            }
+        }
+
+        public void BeginReport(Exception exc)
+        {
+        }
+
+        public void Dispose()
+        {
+            _instance = null;
+        }
+    }
+}
