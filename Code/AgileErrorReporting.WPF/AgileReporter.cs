8e62bea466d0f24ea0d6c0429dd0ee1f58521a09
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-07-05 00:19:21 +0300

    added wpf integration

---
+++ Code/AgileErrorReporting.WPF/AgileReporter.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace AgileErrorReporting.WPF
+{
+    public class AgileReporter
+    {
+        public static void Hook()
+        {
+            var app = Application.Current;
+
+            AgileErrorReporting.AgileReporter.Init();
+            app.Exit += OnExit;
+            app.DispatcherUnhandledException += OnException;
+        }
+
+        private static void OnException(object sender, DispatcherUnhandledExceptionEventArgs eventArgs)
+        {
+            AgileErrorReporting.AgileReporter.Instance.BeginReport(eventArgs.Exception);
+            
+            eventArgs.Handled = true;
+        }
+
+        private static void OnExit(object sender, ExitEventArgs exitEventArgs)
+        {
+            var app = Application.Current;
+
+            app.DispatcherUnhandledException -= OnException;
+            app.Exit -= OnExit;
+            AgileErrorReporting.AgileReporter.Dispose();
+        }
+    }
+}
